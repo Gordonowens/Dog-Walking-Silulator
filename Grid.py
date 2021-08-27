b7b24ac828a9553077260283b7f7578213487f66
Gordonowens <gordon.owens7@gmail.com>
2021-08-27 15:47:35 +1000

    some functionality

---
+++ Grid.py
@@ -0,0 +1,15 @@
+
+class Grid():
+
+    def __init__(self, grid):
+
+        self.grid = grid
+
+    def getGrid(self):
+        return self.grid
+
+    def updateGrid(self):
+        pass
+
+    def setGrid(self, grid):
+        self.grid = grid
---
+++ main.py
@@ -0,0 +1,94 @@
+import pygame
+# from sprites import *
+from config import *
+import sys
+from Node import Node
+import math
+from queue import PriorityQueue
+
+from Player import Player
+from NPC import NPC
+from Grid import*
+
+
+def make_grid(width):
+    grid = []
+    gameGrid = Grid([])
+    gap = width // len(tilemap)
+    for i in range(len(tilemap)):
+        grid.append([])
+        for j in range(len(tilemap)):
+            if(tilemap[j][i] == "B"):
+                node = Node(i, j, gap, len(tilemap), BLACK)
+                grid[i].append(node)
+
+            elif(tilemap[j][i] == "E"):
+                node = Node(i, j, gap, len(tilemap), YELLOW)
+
+                ENEMY.append(NPC(gameGrid, node))
+                grid[i].append(node)
+
+            elif(tilemap[j][i] == "P"):
+                node = Node(i, j, gap, len(tilemap), GREY)
+                PLAYER.append(Player(gameGrid, node))
+                grid[i].append(node)
+
+
+            else:
+                node = Node(i, j, gap, len(tilemap))
+                grid[i].append(node)
+
+        gameGrid.setGrid(grid)
+        #print(grid)
+    return gameGrid
+
+
+def draw(win, grid, width):
+    win.fill(WHITE)
+
+    for row in grid:
+        for node in row:
+            node.draw(win)
+
+    draw_grid(win, width)
+    pygame.display.update()
+
+
+def draw_grid(win, width):
+    gap = width // len(tilemap)
+    for i in range(len(tilemap)):
+        pygame.draw.line(win, GREY, (0, i * gap), (width, i * gap))
+        for j in range(len(tilemap)):
+            pygame.draw.line(win, GREY, (j * gap, 0), (j * gap, width))
+
+
+def main():
+    grid = make_grid(WIDTH)
+
+
+    start = None
+    end = None
+
+    run = True
+    while run:
+        draw(WIN, grid.getGrid(), WIDTH)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    for row in grid.getGrid():
+                        for node in row:
+                            node.update_neighbors(grid.getGrid())
+
+                    #ENEMY[0].updatePath(algorithm(grid, ENEMY[0].getNode(), PLAYER[0].getNode()))
+
+        PLAYER[0].update()
+        ENEMY[0].update()
+
+
+    pygame.quit()
+
+
+main()
